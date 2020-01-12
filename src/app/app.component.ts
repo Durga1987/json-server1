12c9ff2fea4bf35c6069dfa1a4353d2ed8630bb5
Durga Prasad <durga.amperayani@gmail.com>
2020-01-12 21:54:36 +0530

    file save functionality added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { RestService } from './rest.service';
 import {employees} from './employees';
+import * as _ from 'lodash'
+import { saveAs } from 'file-saver';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -11,20 +13,40 @@ export class AppComponent implements OnInit {
   employees:employees[]=[];
   columns = ["Employee Id", "First Name","Last Name","Email"];
   index = ["id","first_name","last_name","email"];
+  isFileSave:boolean = false;
   constructor(private rs : RestService){
     
   }
   ngOnInit(){
-    this.rs.getUsers().subscribe((response)=>{
-      this.employees = response;
-    });
+  this.getUsers();
   }
-  saveUser() { 
-    const emp = { id: '4', first_name: 'amperayani', last_name: 'durga', email: 'ampdurga@gmail.com'};
-    this.rs.saveUser(emp).subscribe((result) => { 
-    console.log(result); 
-    }, (err) => { 
-    console.log(err); 
-    }); 
+    saveUser(values) { 
+    const emp = { id: this.employees.length+1, first_name: values.first_name,
+       last_name: values.last_name, email: values.email};
+    const objValue =  _.find(this.employees, { 'first_name': values.first_name, 'last_name': values.last_name,
+        'email' : values.email });
+       if(!objValue.id) {
+            this.rs.saveUser(emp).subscribe((result) => { 
+                console.log(result); 
+                this.getUsers();
+            }, (err) => { 
+                console.log(err); 
+                this.saveToFileSystem(err);
+
+            }); 
+       }
+       else{
+         this.saveToFileSystem("id: "+objValue.id + " already Exists");
+       }
+    }
+    getUsers(){
+      this.rs.getUsers().subscribe((response)=>{
+        this.employees = response;
+      });
+    }
+    private saveToFileSystem(response) {
+      const filename = "errorlog";
+      const blob = new Blob([response], { type: 'text/plain' });
+      saveAs(blob, filename);
     }
 }
